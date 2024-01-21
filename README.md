50451a74480f90100fc9fccdc60a26de4daf5d1f
Raui Ghazaleh <rj.ghazaleh@gmail.com>
2024-01-21 22:08:17 +0100

    Implement a blinkind LED

---
+++ README.md
@@ -0,0 +1,7 @@
+# STM32F401 & [Embassy](https://embassy.dev/)
+Playing around with an STM32F401.
+
+## Blinking LED
+> cargo run --release --bin hello_world
+
+Implemented a blinking LED (the `Hello World!` of the micro cosmos).
---
+++ src/bin/hello_world.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+
+    loop {
+        info!("high");
+        led.set_high();
+        Timer::after_millis(500).await;
+
+        info!("low");
+        led.set_low();
+        Timer::after_millis(500).await;
+    }
+}
